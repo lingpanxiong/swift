cd55df90cf9f974b900af58eb0e9ed6303586a5f
iront <539966608@qq.com>
2016-10-28 00:39:38 +0800

    new for swift
    new…

---
+++ swiftls/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  swiftls
+//
+//  Created by iront on 5/21/16.
+//  Copyright © 2016 iront. All rights reserved.
+//
+
+//learning note of swift
+import Foundation
+var shoppinglist = ["catfish","water","tulips","blue paint"]
+print (shoppinglist)
+var booklist: Dictionary<Int,String>=[1:"nice",2:"double fish"]
+let str=[21,25,28,98,68]
+func returnfifteen() -> Int {
+    var y = 10
+        func add()
+        {
+            y += 5
+        }
+    add()
+    return y
+}
+
+print( returnfifteen() )
